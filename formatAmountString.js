bbe443fb3a98f30c1820494d2f1dbfe6479f2027
Ajith Nair <ajith.jayan20@gmail.com>
2019-04-11 14:14:20 +0530

    changes

--- formatAmountString.js
+++ formatAmountString.js
@@ -1,13 +1,33 @@
-function formatAmountString(inputnumber) {
-    var num = inputnumber.toString();
+function formatAmountString(inputnumber,format, precision) {
+    
+    precision = inputnumber.toString().split(".")[0].length + precision;    //get the precision i.e total number of digits to be displayed
+    
+    var num = inputnumber.toPrecision(precision);
+    
+    var numstr = num.toString().split(".");
+    var characteristic = numstr[0];
+    var mantissa = numstr[1];
+
     var formattednum = '';
 
-    for (var i = num.length-1, counter=1; i >= 0; i--) {
+    //When L ',' is inserted as 1,00,000 and When M ',' is inserted as 1,000,000
+    for (var i = characteristic.length - 1, counter = 1; i >= 0; i--) {
+
+        formattednum = formattednum + characteristic.charAt(i);
+
+        if (format === 'L') {
+            if (counter % 2 === 1 && counter != 1 && counter != characteristic.length)
+                formattednum = formattednum + ',';
+        }
+        else if (format === 'M') {
+            if (counter % 3 === 0 && counter != characteristic.length)
+                formattednum = formattednum + ',';
+        }
         
-        formattednum = formattednum + num.charAt(i);
-        if (counter % 2 === 1 && counter != 1 && counter != num.length)
-            formattednum = formattednum + ',';
         counter++;
     }
-    return formattednum.split("").reverse().join("");
+    if (mantissa === undefined)
+        return formattednum.split("").reverse().join("");
+    else
+        return formattednum.split("").reverse().join("") + '.' + mantissa;
 }
